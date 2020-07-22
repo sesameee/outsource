2a5af86537b5e79a7d85122bf72a5a0b63f67128
linda <linda.chen@noname.com>
2020-07-22 14:05:32 +0800

    新增一個範例

--- types/apis/banner.ts
+++ types/apis/banner.ts
@@ -3,5 +3,9 @@ export interface BannerData {
     contentType: string
     sourceUrl: string
     desc: string
+    /**
+     * @description 連結
+     * @examples ["https://avatars0.githubusercontent.com/u/19217384?s=460&u=fe86581d6e255b21a44f66626875c1bf634ee902&v=4", "上海", "北京"]
+     */
     linkUrl: string
 }
