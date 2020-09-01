6b0e532032dc6da89c9d59e343a5a87a1a087882
linda <linda.chen@noname.com>
2020-09-01 12:34:36 +0800

    修正問題

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,8 +16,27 @@ class MyDocument extends Document {
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
                     <meta name="keywords" content="Breeze Online" />
                     <meta name="description" content="Breeze Online - Breeze Fashion" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-TLR7RTC');
+          `,
+                        }}
+                    />
                 </Head>
                 <body>
+                    <noscript>
+                        <iframe
+                            src="https://www.googletagmanager.com/ns.html?id=GTM-TLR7RTC"
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        ></iframe>
+                    </noscript>
                     <Main />
                     <NextScript />
                     <script src="https://js.tappaysdk.com/tpdirect/v5.2.0"></script>
