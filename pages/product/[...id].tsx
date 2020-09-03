7ee9caad3f937678ac6985b368d6c589d3176e9c
Linda <linda.chen@linda>
2020-09-03 21:01:53 +0800

    fixed crash

--- pages/product/[...id].tsx
+++ pages/product/[...id].tsx
@@ -56,6 +56,7 @@ const Product: NextPage<any> = ({ router }: CategoryProps): JSX.Element => {
                 setSubSpecList(info[spec1]['subSpecList'])
             }
         }
+        setSpec2(0)
     }, [spec1, info])
     const spec1Name = info && info[spec1] && info[spec1].name
     const spec2Name = subSpecList && subSpecList[spec2] && subSpecList[spec2].name
